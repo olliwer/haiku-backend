a31f48c9b15471ea10f9b88713f2a2d8d13eddbd
Olli Rissanen <olli.rissanen@futurice.com>
2015-02-06 12:22:16 +0200

    Added JSON requirement to home path

--- server.js
+++ server.js
@@ -2,20 +2,27 @@ var express = require('express');
  
 var app = express();
 
-app.get('/', function(request, response) {
-  response.send('Hello Wappu! Try /instructions.');
+app.get('/', function(req, res) {
+	if (req.is('json')) {
+    	res.send('Greetings, traveler! Try /instructions.');
+	} else {
+		res.send('Try JSON.');
+	}
 });
  
 app.get('/secret', function(req, res) {
-    res.send([{key:'wappu15'}, {type:'AES 128'}, {message:'YMvASzfz3II4WtuMlrOUQkQ2C2XLxcODRcMhG/T117+VAu42v04/5fGqYBz2+ytSNyDGtRorWCqwIqFDF63H4g=='}]);
+    res.json([{key:'wappu15'}, {message:'YMvASzfz3II4WtuMlrOUQkQ2C2XLxcODRcMhG/T117+VAu42v04/' +
+    	'5fGqYBz2+ytSNyDGtRorWCqwIqFDF63H4g=='}]);
 });
 
 app.get('/instructions', function(req, res) {
-    res.send([{message:'Try to find a secret API method.'}]);
+    res.send('There might be a secret API method.');
 });
 
 app.get('/challenge', function(req, res) {
-	res.send([{name:'Create a web application that takes in the following JSON as a POST request. Further instructions follow. E-mail the URL to <e-mail>.'}]);
+	res.json([{message: 'Create a web application that takes in the following JSON as a POST ' +
+		'request. Further instructions follow. E-mail the URL to <e-mail>.', 
+		json: {"content": {"message":"string","asd":"string","f":"string"}}}]);
 });
 
 app.listen(process.env.PORT || 3000);
