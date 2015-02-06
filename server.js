06d5096d8d5715a2cc47c24d7809176af9c877c1
Olli Rissanen <olli.rissanen@futurice.com>
2015-02-06 23:39:10 +0200

    Added postgresql

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var pg = require('pg');
  
 var app = express();
 
@@ -36,7 +37,18 @@ app.get('/challenge', function(req, res) {
 });
 
 app.get('/challenge/:path', function(req, res) {
-	var path = req.params.path;
+	var path = req.params.path;	
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		client.query('INSERT INTO PARTICIPANTS VALUES (1, "' + path + '")', function(err, result){
+			done();
+			if (err) { 
+				console.error(err); 
+				response.send("Error " + err); 
+			} else { 
+				response.send(result.rows);
+			}
+		});
+	});
 
 });
 
