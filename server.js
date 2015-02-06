c98c90fbb6f4fde3c13441f4553604949ab17892
Olli Rissanen <olli.rissanen@futurice.com>
2015-02-06 22:38:19 +0200

    Require json from every req

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express = require('express');
  
 var app = express();
 
+function acceptsJson(req){
+	return req.accepted.some(function(type) {return type.value === 'application/json';});
+}
+
 app.get('/', function(req, res) {
-	if (req.accepted.some(function(type) {return type.value === 'application/json';})) {
+	if (acceptsJson(req)) {
     	res.send('Greetings, traveler! Try /instructions.');
 	} else {
 		res.send('Try JSON.');
@@ -11,18 +15,29 @@ app.get('/', function(req, res) {
 });
 
 app.get('/instructions', function(req, res) {
-    res.send('Follow the secret path.');
+	if (acceptsJson(req)) {
+    	res.send('Follow the secret path.');
+    }
 });
  
 app.get('/secret', function(req, res) {
-    res.json([{key:'wappu15'}, {message:'YMvASzfz3II4WtuMlrOUQkQ2C2XLxcODRcMhG/T117+VAu42v04/' +
-    	'5fGqYBz2+ytSNyDGtRorWCqwIqFDF63H4g=='}]);
+	if (acceptsJson(req)) {
+    	res.json([{key:'wappu15'}, {message:'YMvASzfz3II4WtuMlrOUQkQ2C2XLxcODRcMhG/T117+VAu42v04/' +
+    		'5fGqYBz2+ytSNyDGtRorWCqwIqFDF63H4g=='}]);
+    }
 });
 
 app.get('/challenge', function(req, res) {
-	res.send('Create a web application that takes in the following JSON as a POST ' +
-		'request. Further instructions follow. E-mail the URL to _e-mail_. \n' + 
-		JSON.stringify({"content": {"message":"string","asd":"string","f":"string"}}, null, 4));
+	if (acceptsJson(req)) {
+		res.send('Create a web application that takes in the following JSON as a POST ' +
+			'request. Further instructions follow. Send the path to the following path: /challenge/path-to-project \n' + 
+			JSON.stringify({"content": {"message":"string","field1":"string","field2":"string"}}, null, 4));
+	}
+});
+
+app.get('/challenge/:path', function(req, res) {
+	var path = req.params.path;
+
 });
 
 app.listen(process.env.PORT || 3000);
