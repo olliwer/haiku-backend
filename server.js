8e53cd83d4e5df1c0693fd0611c6aa5a46284dea
Ville Tainio <ville.tainio@futurice.com>
2015-04-02 16:38:54 +0300

    Add final part of the competition

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var pg = require('pg');
 var bodyParser = require('body-parser');
+var http = require('http');
+var when = require('when');
+var url_parser = require('url');
 
 var app = express();
 
@@ -64,6 +67,68 @@ app.get('/challenge', function(req, res) {
 	}
 });
 
+function isEmpty(value) {
+	return !value || value.trim() == '';
+}
+
+function validateWebServer(url) {
+	var def = when.defer();
+
+	var number1 = Math.floor((Math.random() * 100) + 1);
+	var number2 = Math.floor((Math.random() * 100) + 1);
+	var parser = url_parser.parse(url);
+
+	var data = {
+		number1: number1,
+		number2: number2
+	};
+
+	var dataString = JSON.stringify(data);
+
+	var headers = {
+		'Content-Type': 'application/json'
+	};
+
+	var options = {
+		host: parser.hostname,
+		port: parser.port,
+		path: parser.pathname,
+		method: 'POST',
+		headers: headers
+	};
+
+	var request = http.request(options, function (response) {
+		response.setEncoding('utf-8');
+
+		var responseString = '';
+
+		response.on('data', function(data) {
+			responseString += data;
+		});
+
+		response.on('end', function() {
+			try {
+				resultData = JSON.parse(responseString);
+				if (resultData.result === (number1 + number2)) {
+					return def.resolve(true);
+				} else {
+					return def.resolve(false);
+				}
+			} catch (err) {
+				def.resolve(false);
+			}
+		});
+	});
+
+	request.on('error', function(err) {
+		def.resolve(false);
+	});
+
+	request.write(dataString);
+	request.end();
+
+	return def.promise;
+}
 
 app.post('/challenge', function(req, res) {
 	if (!req.is('json')){
@@ -72,22 +137,28 @@ app.post('/challenge', function(req, res) {
 	var url = req.body.url;
 	var name = req.body.name;
 	var email = req.body.email;
-	if (url && name && email){
-		pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('INSERT INTO PARTICIPANT(path, email, name) VALUES ($1, $2, $3)', [url, name, email], function(err, result){
-			done();
-			if (err) { 
-				console.error(err); 
-				res.json({message: "Caught an error. Please check your query and try again."}); 
-			} else { 
-				res.json({message: "Godspeed. We'll stay in touch."});
-			}
-		});
-	});
-	} else {
-		res.status(400).json({message: "Please provide all values."});
-	}
-
+	if (!isEmpty(url) && !isEmpty(name) && !isEmpty(email)) {
+			validateWebServer(url)
+			.done(function(validation) {
+				if (!validation) {
+					res.json({message: "Your server didn't return the correct value"})
+				} else {
+					pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+						client.query('INSERT INTO PARTICIPANT(path, email, name) VALUES ($1, $2, $3)', [url, name, email], function(err, result){
+							done();
+							if (err) {
+								console.error(err);
+								res.json({message: "Caught an error. Please check your query and try again."});
+							} else {
+								res.json({message: "Godspeed. We'll stay in touch."});
+							}
+						});
+					});
+				}
+			});
+		} else {
+			res.json({message: "Parameters missing"});
+		}
 });
 
 app.listen(process.env.PORT || 3000);
